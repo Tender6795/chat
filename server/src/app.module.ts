184b673d33189ee529e6095d2ff61f6551b5d188
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-02-06 10:39:07 +0200

    Migrate pg=>prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from "@nestjs/sequelize";
-import { ConfigModule } from "@nestjs/config";
-
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,19 +12,23 @@ import { MessageModule } from './message/message.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath:`.${process.env.NODE_ENV}.env`
-  }),
-    SequelizeModule.forRoot({
-      dialect: "postgres",
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database:process.env.DB_NAME,
-      models: [],
-      autoLoadModels: true,
+      envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
-    AuthModule, UserModule, RoomModule, MessageModule],
+    // SequelizeModule.forRoot({
+    //   dialect: "postgres",
+    //   host: process.env.DB_HOST,
+    //   port: Number(process.env.DB_PORT),
+    //   username: process.env.DB_USER,
+    //   password: process.env.DB_PASSWORD,
+    //   database:process.env.DB_NAME,
+    //   models: [],
+    //   autoLoadModels: true,
+    // }),
+    AuthModule,
+    UserModule,
+    RoomModule,
+    MessageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

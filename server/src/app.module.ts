b8bd1b0bf152b0640ba4d32985004047baea68c2
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-02-06 09:14:18 +0200

    Connect to db and chat in progress

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from "@nestjs/sequelize";
+import { ConfigModule } from "@nestjs/config";
+
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MoController } from './s/mo/mo.controller';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { RoomModule } from './room/room.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController, MoController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:`.${process.env.NODE_ENV}.env`
+  }),
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database:process.env.DB_NAME,
+      models: [],
+      autoLoadModels: true,
+    }),
+    AuthModule, UserModule, RoomModule, MessageModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}

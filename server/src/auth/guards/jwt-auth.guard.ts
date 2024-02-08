c379f408c235641c721d2306f7fc188c87b8f933
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-02-08 12:32:26 +0200

    Fix typo

--- server/src/auth/guards/jwt-auth.guard.ts
+++ server/src/auth/guards/jwt-auth.guard.ts
@@ -14,7 +14,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
     ctx: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const _isPublic = isPublic(ctx, this.reflector);
-    if (isPublic) {
+    if (_isPublic) {
       return true;
     }
     return super.canActivate(ctx);

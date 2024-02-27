dce04ae8d19347f876a877905e8f838f36da833d
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-02-27 17:33:20 +0200

    Invite to room in progress

--- client/src/components/UserCard/UserCard.tsx
+++ client/src/components/UserCard/UserCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import {
   Button,
@@ -6,8 +6,15 @@ import {
   CardActions,
   CardContent,
   Typography,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import { IUser } from "@/interfaces/auth.interface";
+import { useAppSelector } from "@/store/hooks";
+import { selectAllRooms } from "@/store/slices/allRoomsSlice";
+import { IRoom } from "@/interfaces/rooms.interface";
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 
 interface UserProps {
   user: IUser;
@@ -19,12 +26,28 @@ const UserCard: React.FC<UserProps> = ({
   index,
 }) => {
   const truncatedId = id.length > 10 ? id.slice(0, 10) + "..." : id;
+  const rooms = useAppSelector(selectAllRooms).rooms as IRoom[];
+  const [showRoomSelect, setShowRoomSelect] = useState(false);
+  const [selectedRoom, setSelectedRoom] = useState(rooms.length > 0 ? rooms[0].id : '');
+
+  const handleRoomSelect = () => {
+    setShowRoomSelect(true);
+  }
+
+  const handleCancel = () => {
+    setShowRoomSelect(false);
+  }
+
+  const handleConfirm = () => {
+    //TODO
+    setShowRoomSelect(false);
+  }
 
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: index * 0.2 }} 
+      transition={{ delay: index * 0.2 }}
     >
       <Card
         sx={{
@@ -33,6 +56,7 @@ const UserCard: React.FC<UserProps> = ({
           boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
           backgroundColor: "#E6F7FF",
           marginBottom: 4,
+          minHeight: 200, 
         }}
       >
         <CardContent>
@@ -40,6 +64,7 @@ const UserCard: React.FC<UserProps> = ({
             sx={{
               fontSize: 20,
               textAlign: "center",
+              marginTop: 2, 
             }}
             color="text.secondary"
             gutterBottom
@@ -54,7 +79,28 @@ const UserCard: React.FC<UserProps> = ({
           </Typography>
         </CardContent>
         <CardActions sx={{ justifyContent: "center" }}>
-          <Button size="small">Chating </Button>
+          {!showRoomSelect && (
+            <>
+              <Button size="small" onClick={handleRoomSelect} sx={{ mt: 2 }}>Invite to room</Button>
+              <Button size="small" sx={{ mt: 2 }}>Chating</Button>
+            </>
+          )}
+          {showRoomSelect && (
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Select
+                label="Select room"
+                value={selectedRoom}
+                onChange={(e) => setSelectedRoom(e.target.value)}
+                style={{ marginRight: '10px', minWidth: '200px' }}
+              >
+                {rooms.map(room => (
+                  <MenuItem key={room.id} value={room.id}>{room.name}</MenuItem>
+                ))}
+              </Select>
+              <Button onClick={handleConfirm} style={{ marginRight: '5px' }}><CheckIcon /></Button>
+              <Button onClick={handleCancel}><CloseIcon /></Button>
+            </div>
+          )}
         </CardActions>
       </Card>
     </motion.div>

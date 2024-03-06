45093de37baac95002a1b3840686e83fa1a6671a
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-03-06 19:32:16 +0200

    Add documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParcer from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParcer())
+
+  const config = new DocumentBuilder()
+    .setTitle('Chat Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('chat')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.use(cookieParcer());
 
   app.enableCors();
 
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
   await app.listen(5000);
 }
 bootstrap();

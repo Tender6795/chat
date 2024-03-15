3d73c5f71b0dc211d5e7319bd05da940dea13a27
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-03-15 17:47:59 +0200

    Delete export

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,19 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-    throw new Error("GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET is not defined in environment variables.");
-  }
-export const authOptions = {
+  throw new Error(
+    "GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET is not defined in environment variables."
+  );
+}
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      })
+    }),
   ],
-  
-}
+};
 
-const handler =  NextAuth(authOptions)
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };

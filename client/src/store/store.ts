58c3f87778eca5ace78285ffa2497843988d1f4a
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-03-27 15:17:09 +0200

    Change src

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -6,7 +6,7 @@ import currentRoomSlice from './slices/currentRoomSlice'
 import websocketMiddleware from '@/middlewares/websocketMiddleware'
 import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io('ws://localhost:5000/chat', {
+const socket: Socket = io(process.env.WEBSOCKET_SRC ||'ws://localhost:5000/chat', {
   extraHeaders: {
     Authorization: typeof window !== "undefined" ? localStorage.getItem("token") || '' : '',
   },
